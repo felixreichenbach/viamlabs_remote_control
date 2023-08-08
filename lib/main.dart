aafb67a7c50f189a366411b40300a767d57754a4
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2023-08-08 11:45:56 +0200

    simplified codebase and add disconnect functionality
    - Removed unnecessary code
    - added disconnect button
    - added Roboto font

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:my_viam_robot/screens/base.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:viam_sdk/viam_sdk.dart';
+import 'package:viam_sdk/widgets.dart';
 
 void main() async {
   await dotenv.load();
@@ -17,8 +19,9 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Viam Flutter App',
+        title: 'Viam Remote Control',
         theme: ThemeData(
+          fontFamily: 'RobotoMono',
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         ),
@@ -36,13 +39,7 @@ class MyAppState extends ChangeNotifier {
   late ResourceName cameraName;
 
   void login(String location, String secret) {
-    if (_isLoading) {
-      return;
-    }
-    if (_isLoggedIn) {
-      return;
-    }
-
+    _isLoggedIn = true;
     _isLoading = true;
     notifyListeners();
 
@@ -54,7 +51,7 @@ class MyAppState extends ChangeNotifier {
     robotFut.then((value) {
       _robot = value;
       // Print the available resources
-      print(_robot.resourceNames);
+      //print(_robot.resourceNames);
       final components = _robot.resourceNames
           .where((element) => element.type == resourceTypeComponent);
 
@@ -68,6 +65,13 @@ class MyAppState extends ChangeNotifier {
       notifyListeners();
     });
   }
+
+  void logout() {
+    _robot.close().then((value) {
+      _isLoggedIn = false;
+      notifyListeners();
+    });
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -78,20 +82,39 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
 
     return Scaffold(
-      appBar: AppBar(),
-      body: !appState._isLoggedIn
-          ? const LoginPage()
-          : appState._isLoading
-              ? const Placeholder()
-              : BaseScreen(
-                  base: Base.fromRobot(appState._robot,
-                      'viam_base'), // TODO: Make 'viam_base' dynamic
-                  cameras: appState._robot.resourceNames
-                      .where((element) =>
-                          element.subtype == Camera.subtype.resourceSubtype)
-                      .map((e) => Camera.fromRobot(appState._robot, e.name)),
-                  robot: appState._robot,
-                ),
+      appBar: AppBar(
+        title: const Text('Viam Remote Control'),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: !appState._isLoggedIn
+              ? const LoginPage()
+              : appState._isLoading
+                  ? PlatformCircularProgressIndicator()
+                  : Column(
+                      children: [
+                        ViamBaseScreen(
+                          base: Base.fromRobot(appState._robot,
+                              'viam_base'), // TODO: Make 'viam_base' dynamic
+                          cameras: appState._robot.resourceNames
+                              .where((element) =>
+                                  element.subtype ==
+                                  Camera.subtype.resourceSubtype)
+                              .map((e) =>
+                                  Camera.fromRobot(appState._robot, e.name)),
+                          robotClient: appState._robot,
+                        ),
+                      ],
+                    ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          appState.logout();
+        },
+        tooltip: 'Logout',
+        child: const Icon(Icons.exit_to_app),
+      ),
     );
   }
 }
@@ -158,8 +181,6 @@ class _LoginPageState extends State<LoginPage> {
               ),
             ),
             ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  textStyle: const TextStyle(fontSize: 20)),
               onPressed: () {
                 if (_formKey.currentState!.validate()) {
                   appState.login(
